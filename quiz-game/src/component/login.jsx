39aae8557f97a4db580ab5173cc429e3666e7699
yassinops <yassinfaouar@gmail.com>
2024-08-19 13:14:21 +0100

    tailwindcss
    implementing tailwindcss to the project

---
+++ quiz-game/src/component/login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    
+    render() { 
+        return (
+            <div className='bg-blue-500'>
+                <h1 className='text-blue-300 font-sans'>Login</h1>
+            </div>
+        );
+    }
+}
+ 
+export default LoginForm;
